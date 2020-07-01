74c3eaede9aaf41ed1d9c7d4763d4d560ebeb994
FlowArg <coutureflorence3@gmail.com>
2020-07-01 21:43:33 +0200

    Fixed unzipping jar bug

--- src/main/java/fr/flowarg/flowupdater/minecraft/versions/NewForgeVersion.java
+++ src/main/java/fr/flowarg/flowupdater/minecraft/versions/NewForgeVersion.java
@@ -5,14 +5,19 @@ import static fr.flowarg.flowio.FileUtils.getSHA1;
 
 import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.ProcessBuilder.Redirect;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Objects;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import fr.flowarg.flowio.FileUtils;
 import fr.flowarg.flowlogger.Logger;
@@ -81,9 +86,9 @@ public class NewForgeVersion implements IForgeVersion
                 Files.copy(new URL("https://flowarg.github.io/minecraft/launcher/patches.jar").openStream(), patches.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
                 this.logger.info("Applying patches...");
-                FileUtils.unzipJar(tempInstallerDir.getAbsolutePath(), install.getAbsolutePath());
+                this.unzipJar(tempInstallerDir, install);
                 this.cleaningInstaller(tempInstallerDir);
-                FileUtils.unzipJar(tempInstallerDir.getAbsolutePath(), patches.getAbsolutePath());
+                this.unzipJar(tempInstallerDir, patches);
                 this.logger.info("Repack installer...");
                 this.packPatchedInstaller(tempDir, tempInstallerDir);
                 patches.delete();
@@ -120,6 +125,54 @@ public class NewForgeVersion implements IForgeVersion
         ZipUtils.INSTANCE.compressFiles(tempInstallerDir.listFiles(), output);
         Files.move(output.toPath(), new File(output.getAbsolutePath().replace(".zip", ".jar")).toPath(), StandardCopyOption.REPLACE_EXISTING);
         tempInstallerDir.delete();
+    }
+    
+    private void unzipJar(final File destinationDir, final File jarFile) throws IOException
+    {
+        final JarFile jar = new JarFile(jarFile);
+
+        for (Enumeration<JarEntry> enums = jar.entries(); enums.hasMoreElements(); )
+        {
+            final JarEntry entry = enums.nextElement();
+
+            final String fileName = destinationDir + File.separator + entry.getName();
+            final File   file     = new File(fileName);
+
+            if (fileName.endsWith("/")) file.mkdirs();
+        }
+
+        for (Enumeration<JarEntry> enums = jar.entries(); enums.hasMoreElements(); )
+        {
+            final JarEntry entry = enums.nextElement();
+
+            final String fileName = destinationDir + File.separator + entry.getName();
+            final File   file     = new File(fileName);
+
+            if (!fileName.endsWith("/"))
+            {
+                if (fileName.endsWith(".lzma"))
+                {
+                    new File(destinationDir, "data").mkdir();
+                    final InputStream stream = jar.getInputStream(entry);
+                    Files.copy(stream, new File(destinationDir, entry.getName()).toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    stream.close();
+                }
+                else
+                {
+                    final InputStream      is  = jar.getInputStream(entry);
+                    final FileOutputStream fos = new FileOutputStream(file);
+
+                    while (is.available() > 0)
+                        fos.write(is.read());
+
+                    fos.close();
+                    is.close();
+                }
+                jar.getInputStream(entry).close();
+            }
+        }
+
+        jar.close();
     }
 
     public Logger getLogger()
