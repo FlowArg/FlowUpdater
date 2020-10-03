9c6ab7dacfa2d8a18807fa41c2d2a2073bb3603f
FlowArg <coutureflorence3@gmail.com>
2020-10-03 16:55:30 +0200

    Started CurseForgePLugin

---
+++ CurseForgePlugin/src/main/java/fr/flowarg/flowupdater/curseforgeplugin/CurseForgePlugin.java
@@ -0,0 +1,18 @@
+package fr.flowarg.flowupdater.curseforgeplugin;
+
+import fr.flowarg.pluginloaderapi.plugin.Plugin;
+
+public class CurseForgePlugin extends Plugin
+{
+    @Override
+    public void onStart()
+    {
+
+    }
+
+    @Override
+    public void onStop()
+    {
+
+    }
+}
