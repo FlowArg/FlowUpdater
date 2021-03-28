7c7a04640248599c3847332520876c83d635561d
FlowArg <coutureflorence3@gmail.com>
2021-03-28 17:41:55 +0200

    IProgressCallback methods are now default

--- src/main/java/fr/flowarg/flowupdater/download/IProgressCallback.java
+++ src/main/java/fr/flowarg/flowupdater/download/IProgressCallback.java
@@ -9,18 +9,18 @@ public interface IProgressCallback
      * This method is called at {@link FlowUpdater} initialization.
      * @param logger {@link ILogger} of FlowUpdater instance.
      */
-    void init(ILogger logger);
+    default void init(ILogger logger) {}
 
     /**
      * This method is called when a step started.
      * @param step Actual {@link Step}.
      */
-    void step(Step step);
+    default void step(Step step) {}
 
     /**
      * This method is called when a new file is downloaded.
      * @param downloaded Number of downloaded/checked bytes.
      * @param max Total bytes to download/check.
      */
-    void update(long downloaded, long max);
+    default void update(long downloaded, long max) {}
 }
