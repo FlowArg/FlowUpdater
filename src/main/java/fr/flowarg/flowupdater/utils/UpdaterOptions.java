65656fb3e433111de6aa742bd29387b7978e9ac0
FlowArg <coutureflorence3@gmail.com>
2021-07-05 22:21:03 +0200

    Fixed JavaDOc in UpdaterOptions

--- src/main/java/fr/flowarg/flowupdater/utils/UpdaterOptions.java
+++ src/main/java/fr/flowarg/flowupdater/utils/UpdaterOptions.java
@@ -10,46 +10,18 @@ import java.util.Random;
 /**
  * Represent some settings for FlowUpdater
  *
- * @author flow
+ * @author FlowArg
  */
 public class UpdaterOptions
 {
     public static final UpdaterOptions DEFAULT = new UpdaterOptions(true, false, false, false, false, new Random().nextInt(2) + 2, new ExternalFileDeleter());
 
-    /**
-     * Is the read silent
-     */
     private final boolean silentRead;
-
-    /**
-     * Is the server must be downloaded
-     */
     private final boolean downloadServer;
-
-    /**
-     * Re-extract natives at each updates ?
-     * Today, this option isn't useful because {@link VanillaDownloader} checks automatically if all natives are correctly extracted.
-     */
     private final boolean reExtractNatives;
-
-    /**
-     * Enable CurseForgePlugin (CPF) ?
-     */
     private final boolean enableCurseForgePlugin;
-
-    /**
-     * Enable OptifineDownloaderPlugin (ODP) ?
-     */
     private final boolean enableOptifineDownloaderPlugin;
-
-    /**
-     * Number of threads used to download assets.
-     */
     private final int nmbrThreadsForAssets;
-
-    /**
-     * The external file deleter is used to check if some external files need to be downloaded.
-     */
     private final IFileDeleter externalFileDeleter;
 
     private UpdaterOptions(boolean silentRead, boolean reExtractNatives, boolean enableCurseForgePlugin, boolean enableOptifineDownloaderPlugin, boolean downloadServer, int nmbrThreadsForAssets, IFileDeleter externalFileDeleter)
@@ -63,36 +35,65 @@ public class UpdaterOptions
         this.externalFileDeleter = externalFileDeleter;
     }
 
+    /**
+     * Disable some debug logs on Minecraft json's parsing.
+     * Default: true
+     */
     public boolean isSilentRead()
     {
         return this.silentRead;
     }
 
+    /**
+     * If this option is set to true, {@link fr.flowarg.flowupdater.FlowUpdater} will download the Minecraft Server.
+     * Default: false
+     */
     public boolean isDownloadServer()
     {
         return this.downloadServer;
     }
 
+    /**
+     * Re-extract natives at each updates?
+     * Today, this option isn't useful because {@link VanillaDownloader} checks automatically if all natives are correctly extracted.
+     * Default: false
+     */
     public boolean isReExtractNatives()
     {
         return this.reExtractNatives;
     }
 
+    /**
+     * Enable CurseForgePlugin (CFP)?
+     * Default: false
+     */
     public boolean isEnableCurseForgePlugin()
     {
         return this.enableCurseForgePlugin;
     }
 
+    /**
+     * Enable OptifineDownloaderPlugin (ODP)?
+     * Default: false
+     */
     public boolean isEnableOptifineDownloaderPlugin()
     {
         return this.enableOptifineDownloaderPlugin;
     }
 
+    /**
+     * Number of threads used to download assets.
+     * Default: 2 or 3 (random)
+     */
     public int getNmbrThreadsForAssets()
     {
         return this.nmbrThreadsForAssets;
     }
 
+    /**
+     * The external file deleter is used to check if some external files need to be downloaded.
+     * Default: {@link fr.flowarg.flowupdater.utils.ExternalFileDeleter}
+     */
     public IFileDeleter getExternalFileDeleter()
     {
         return this.externalFileDeleter;
@@ -105,7 +106,7 @@ public class UpdaterOptions
         private final BuilderArgument<Boolean> enableCurseForgePluginArgument = new BuilderArgument<>("EnableCurseForgePlugin", () -> false).optional();
         private final BuilderArgument<Boolean> enableOptifineDownloaderPluginArgument = new BuilderArgument<>("EnableOptifineDownloaderPlugin", () -> false).optional();
         private final BuilderArgument<Boolean> downloadServerArgument = new BuilderArgument<>("DownloadServer", () -> false).optional();
-        private final BuilderArgument<Integer> nmbrThreadsForAssetsArgument = new BuilderArgument<>("Number of Threads for assets", () -> 2).optional();
+        private final BuilderArgument<Integer> nmbrThreadsForAssetsArgument = new BuilderArgument<>("Number of Threads for assets", () -> new Random().nextInt(2) + 2).optional();
         private final BuilderArgument<IFileDeleter> externalFileDeleterArgument = new BuilderArgument<IFileDeleter>("External FileDeleter", ExternalFileDeleter::new).optional();
 
         public UpdaterOptionsBuilder withSilentRead(boolean silentRead)
